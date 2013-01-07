ca5291a6c4254376fa4a1ecc93d6eedb9b315f6c
Dushyant Rijhwani <dushyantrijhwani@gmail.com>
2013-01-07 23:01:28 +0400

    adding infinite scrolling and modals to load images;

--- flickr/__init__.py
+++ flickr/__init__.py
@@ -8,58 +8,60 @@ from flickr.querysets import FlickrPhotosQuerySet
 
 class BaseFlickrApi(object):
 
-	def __init__(self, method='', format='json', *args, **kwargs):
-		self.base_url = settings.FLICKR_API_URL
-		self.api_key = settings.FLICKR_API_KEY
-		self.method = method
-		self.format = format
-		self.nojsoncallback = 1
-		self.non_query_params = ['non_query_params', 'base_url']
+    def __init__(self, method='', format='json', *args, **kwargs):
+        self.base_url = settings.FLICKR_API_URL
+        self.api_key = settings.FLICKR_API_KEY
+        self.method = method
+        self.format = format
+        self.nojsoncallback = 1
+        self.non_query_params = ['non_query_params', 'base_url']
 
-	def build_call_url(self):
-		"""
-		"""
-		attrs = self.__dict__.copy()
-		for param in self.non_query_params:
-			attrs.pop(param)
+    def build_call_url(self):
+        """
+        """
+        attrs = self.__dict__.copy()
+        for param in self.non_query_params:
+            attrs.pop(param)
 
-		query_string = urllib.urlencode(attrs)
+        query_string = urllib.urlencode(attrs)
 
-		return '%s?%s' % (self.base_url, query_string)
+        return '%s?%s' % (self.base_url, query_string)
 
-	def __call__(self):
-		url = self.build_call_url()
+    def __call__(self):
+        url = self.build_call_url()
 
-		r = requests.get(url)
+        r = requests.get(url)
 
-		return r
+        return r
 
 
 class FlickrPhotoSearch(BaseFlickrApi):
 
-	def __init__(self, woe_id='', sort='interestingness-desc', *args, **kwargs):
-		super(FlickrPhotoSearch, self).__init__(method='flickr.photos.search', *args, **kwargs)
+    def __init__(self, woe_id='', sort='interestingness-desc', page=1, per_page=100, *args, **kwargs):
+        super(FlickrPhotoSearch, self).__init__(method='flickr.photos.search', *args, **kwargs)
 
-		self.woe_id = woe_id
-		self.sort = sort
+        self.woe_id = woe_id
+        self.sort = sort
+        self.per_page = per_page
+        self.page = page
 
-	def __call__(self):
-		response = super(FlickrPhotoSearch, self).__call__()
+    def __call__(self):
+        response = super(FlickrPhotoSearch, self).__call__()
 
-		content = response.json()
+        content = response.json()
 
-		return FlickrPhotosQuerySet(content=content)
+        return FlickrPhotosQuerySet(content=content)
 
 class FlickrPlacesFind(BaseFlickrApi):
 
-	def __init__(self, query='', *args, **kwargs):
-		super(FlickrPlacesFind, self).__init__(method='flickr.places.find', *args, **kwargs)
+    def __init__(self, query='', *args, **kwargs):
+        super(FlickrPlacesFind, self).__init__(method='flickr.places.find', *args, **kwargs)
 
-		self.query = query
+        self.query = query
 
-	def __call__(self):
-		response = super(FlickrPlacesFind, self).__call__()
+    def __call__(self):
+        response = super(FlickrPlacesFind, self).__call__()
 
-		content = response.json()
+        content = response.json()
 
-		return FlickrPlacesQuerySet(content=content)
+        return FlickrPlacesQuerySet(content=content)
--- images/views.py
+++ images/views.py
@@ -1,3 +1,5 @@
+import urllib
+
 from django.shortcuts import render
 
 from flickr import FlickrPlacesFind
@@ -6,28 +8,35 @@ from flickr import FlickrPhotoSearch
 from images.forms import SearchForm
 
 def home(request):
-	template_context ={
-		'form': SearchForm()
-	}
-	return render(request, 'home.html', template_context)
+    template_context ={
+        'form': SearchForm()
+    }
+    return render(request, 'home.html', template_context)
 
 def search(request):
-	form = SearchForm(request.GET.copy())
-	template_context = {
-		'form': form,
-	}
+    form = SearchForm(request.GET.copy())
+    template_context = {
+        'form': form,
+    }
 
-	if form.is_valid():
-		places = FlickrPlacesFind(**form.cleaned_data)()
-		try:
-			photos = FlickrPhotoSearch(woe_id=places[0].woeid)()
-		except IndexError:
-			photos = []
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError, TypeError:
+        page = 1
 
-		template_context.update({
-			'places': places,
-			'photos': photos,
-		})
+    if form.is_valid():
+        places = FlickrPlacesFind(**form.cleaned_data)()
+        try:
+            photos = FlickrPhotoSearch(woe_id=places[0].woeid, page=page)()
+        except IndexError:
+            photos = []
 
-	return render(request, 'images/list.html', template_context)
+        template_context.update({
+            'places': places,
+            'photos': photos,
+            'query': urllib.urlencode(form.cleaned_data),
+            'page': page,
+        })
+
+    return render(request, 'images/list.html', template_context)
 
